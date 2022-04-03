553a94ef3daa705f043c329ea9b80c024eb57669
VitorTz <vitor.ftz@protonmail.com>
2022-04-03 09:31:37 -0300

    Update

--- Jogos/Pong/src/Constants.java
+++ Jogos/Pong/src/Constants.java
@@ -7,7 +7,8 @@ class Constants {
     public static final String WINDOW_TITLE = "Pong!";
     public static final boolean IS_WINDOW_VISIBLE = true;
     public static final boolean IS_WINDOW_RESIZABLE = false;
-    public static final Color WINDOW_BG_COLOR = Color.DARK_GRAY;
+    private static final float[] HSB_COLOR = Color.RGBtoHSB(24, 24, 24, new float[3]);
+    public static final Color WINDOW_BG_COLOR = Color.getHSBColor(HSB_COLOR[0], HSB_COLOR[1], HSB_COLOR[2]);
     public static final int SLEEP_TIME = 30;
 
 }
---
+++ algoritmos/src/ordenacao/MergeSort.java
@@ -0,0 +1,14 @@
+package ordenacao;
+import java.util.List;
+
+class MergeSort {
+
+    public static void mergeSort(int[] list){
+
+    }
+
+    private static void merge(){
+
+    }
+
+}
